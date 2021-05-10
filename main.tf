65e414cf166b22f6ac5d238ddd6b62bc9544fe93
RaduLupan <github@lupan.ca>
2021-05-10 16:27:31 -0400

    Added provider, locals section and switched arguments to variable values

--- main.tf
+++ main.tf
@@ -1,7 +1,26 @@
+provider "aws" {
+  region = var.region
+}
+
+# Calculated local values.
+locals {
+  vpc_id = data.aws_subnet.selected.vpc_id
+
+  common_tags = {
+    terraform   = true
+    environment = var.environment
+  }
+}
+
+# Use this data source to retrieve details about a specific VPC subnet.
+data "aws_subnet" "selected" {
+  id = var.subnet_ids[0]
+}
+
 resource "aws_fsx_windows_file_system" "main" {
-  active_directory_id = aws_directory_service_directory.example.id
-  kms_key_id          = aws_kms_key.example.arn
-  storage_capacity    = 300
-  subnet_ids          = [aws_subnet.example.id]
-  throughput_capacity = 1024
+  active_directory_id = var.active_directory_id
+  storage_capacity    = var.storage_capacity_gb
+  deployment_type     = var.deployment_type
+  subnet_ids          = var.subnet_ids
+  throughput_capacity = var.throughput_capacity
 }
