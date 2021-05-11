b34f7646fb83acd699eb16fe2aef7f459f322691
RaduLupan <github@lupan.ca>
2021-05-11 09:59:00 -0400

    Added locals

--- configure/main.tf
+++ configure/main.tf
@@ -4,6 +4,16 @@ provider "aws" {
 
 # Calculated local values.
 locals {
-  
+  vpc_id = data.aws_subnet.selected.vpc_id
+
+  ami_id            = var.ami_id == null ? data.aws_ami.windows2019[0].id : var.ami_id
+  count_ami_win2019 = var.ami_id == null ? 1 : 0
+
+  any_port     = 0
+  any_protocol = "-1"
+  tcp_protocol = "tcp"
+  all_ips      = ["0.0.0.0/0"]
+
+  rdp_port = 3389 
 }
 
