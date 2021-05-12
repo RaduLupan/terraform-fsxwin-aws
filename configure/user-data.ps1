be9b943ed4940dbe3be563f24f950cb51ea491f0
RaduLupan <github@lupan.ca>
2021-05-12 11:18:36 -0400

    Installed AWS CLI v2 and a couple of Windows features.

--- configure/user-data.ps1
+++ configure/user-data.ps1
@@ -1,3 +1,10 @@
 <powershell>
+# Install AWS CLI v2
+$cmd="msiexec.exe /i https://awscli.amazonaws.com/AWSCLIV2.msi /quiet"
+Invoke-Expression -Command $cmd
+
+# Remote Server Administration Tools for AD PowerShell and DNS
+Install-WindowsFeature RSAT-AD-PowerShell, RSAT-DNS-Server
+ 
 Rename-Computer -NewName "${computer_name}" -Force
 </powershell>
