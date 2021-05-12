03967145c8c3d1cccec32e9345636ef3a408dbc5
RaduLupan <github@lupan.ca>
2021-05-12 11:53:16 -0400

    Added CLI command to associate file system alias to existing file system.

--- configure/user-data.ps1
+++ configure/user-data.ps1
@@ -1,10 +1,16 @@
 <powershell>
-# Install AWS CLI v2
+$fileSystemId="${file_system_id}"
+$fileSystemAlias="${file_system_alias}"
+# Install AWS CLI v2.
 $cmd="msiexec.exe /i https://awscli.amazonaws.com/AWSCLIV2.msi /quiet"
 Invoke-Expression -Command $cmd
 
-# Remote Server Administration Tools for AD PowerShell and DNS
+# Remote Server Administration Tools for AD PowerShell and DNS.
 Install-WindowsFeature RSAT-AD-PowerShell, RSAT-DNS-Server
- 
+
+# Associate alias to existing FSx file system. 
+$cmd="aws fsx associate-file-system-aliases --file-system-id $fileSystemId --aliases $fileSystemAlias"
+Invoke-Expression -Command $cmd
+
 Rename-Computer -NewName "${computer_name}" -Force
 </powershell>
